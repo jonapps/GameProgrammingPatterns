407236d5b9c27b1f6c8fa52c313fe539d0db1201
Jonathan Wiemers <hi@jonathanwiemers.com>
2015-03-20 14:08:45 +0100

    init SFML

---
+++ JGerdesJWiemers/JGerdesJWiemers/Game/Game.cs
@@ -0,0 +1,53 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JGerdesJWiemers.GameIternal
+{
+    class Game
+    {
+        private RenderWindow window;
+        /// <summary>
+        /// Starts the app
+        /// </summary>
+        public void start()
+        {
+            this.window = new RenderWindow(new VideoMode(800, 600),"JGerdesJwiemers");
+            this.run();
+
+        }
+
+        /// <summary>
+        /// Close the window when OnClose event is received
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnClose(object sender, EventArgs e)
+        {
+            RenderWindow window = (RenderWindow)sender;
+            window.Close();
+        }
+
+        /// <summary>
+        /// Where the gameloop takes place
+        /// </summary>
+        private void run()
+        {
+            CircleShape cs = new CircleShape(100.0f);
+            cs.FillColor = Color.Green;
+            window.Closed += this.OnClose;
+            window.SetActive();
+            while (this.window.IsOpen())
+            {
+                window.Clear();
+                window.DispatchEvents();
+                window.Draw(cs);
+                window.Display();
+            }
+        }
+    }
+}
---
+++ JGerdesJWiemers/JGerdesJWiemers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.Window;
+using JGerdesJWiemers.GameIternal;
+
+namespace JGerdesJWiemers
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            new Game().start();
+        }
+    }
+}
