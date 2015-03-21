dd7c00d5e6aafc67fef381aa6f1f1eda41ce3633
Jonathan Wiemers <hi@jonathanwiemers.com>
2015-03-21 22:07:39 +0100

    changed to more senseful structure

--- JGerdesJWiemers/JGerdesJWiemers/App/Game.cs
+++ JGerdesJWiemers/JGerdesJWiemers/App/Game.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace JGerdesJWiemers.GameIternal
+namespace JGerdesJWiemers.App
 {
     class Game
     {
--- JGerdesJWiemers/JGerdesJWiemers/App/Internal/Program.cs
+++ JGerdesJWiemers/JGerdesJWiemers/App/Internal/Program.cs
@@ -5,14 +5,12 @@ using System.Text;
 using System.Threading.Tasks;
 using SFML.Graphics;
 using SFML.Window;
-using JGerdesJWiemers.GameIternal;
+using JGerdesJWiemers.App;
 
-namespace JGerdesJWiemers
+namespace JGerdesJWiemers.App.Internal
 {
     class Program
-    {
-
-        
+    { 
         static void Main(string[] args)
         {
             new Game().Start();
