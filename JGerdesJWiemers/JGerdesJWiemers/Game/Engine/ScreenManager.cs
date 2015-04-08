5af8cbd7de455bbeecec3a59c9b62e1d3e51b74c
Jonas Gerdes <jonas.gerdes@gmx.net>
2015-04-08 20:13:11 +0200

    #1 - added ScreenManager

---
+++ JGerdesJWiemers/JGerdesJWiemers/Game/Engine/ScreenManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JGerdesJWiemers.Game.Engine
+{
+    class ScreenManager : IScreen
+    {
+        private IScreen _currentScreen;
+
+        public IScreen CurrentScreen
+        {
+            get
+            {
+                return _currentScreen;
+            }
+
+            set
+            {
+                if (value != null)
+                {
+                    _currentScreen = value;
+                }
+            }
+        }
+
+        public void Update()
+        {
+            _currentScreen.Update();
+        }
+
+        public void Render(SFML.Graphics.RenderTarget renderTarget, float extra)
+        {
+            _currentScreen.Render(renderTarget, extra);
+        }
+    }
+}
