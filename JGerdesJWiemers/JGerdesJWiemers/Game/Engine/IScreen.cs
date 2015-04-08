c732295efdec2a092e4c925c85508433478364d4
Jonas Gerdes <jonas.gerdes@gmx.net>
2015-04-08 17:44:21 +0200

    #1 - added interface IScreen

---
+++ JGerdesJWiemers/JGerdesJWiemers/Game/Engine/IScreen.cs
@@ -0,0 +1,24 @@
+﻿using SFML.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JGerdesJWiemers.Game.Engine
+{
+    interface IScreen
+    {
+        /// <summary>
+        /// Updates screen 
+        /// </summary>
+        public void Update();
+
+        /// <summary>
+        /// Renders content of screen on provided render target
+        /// </summary>
+        /// <param name="renderTarget">target to render content of screen to</param>
+        /// <param name="extra">value between 0 and 1 for extrapolation</param>
+        public void Render(RenderTarget renderTarget, float extra);
+    }
+}
