9893c90d5733bbc1ede4af1b69bccf0cbbc3a4fb
Jonas Gerdes <jonas.gerdes@gmx.net>
2015-04-14 09:49:45 +0200

    added entity and gamescreen

---
+++ JGerdesJWiemers/JGerdesJWiemers/Game/Engine/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JGerdesJWiemers.Game.Engine
+{
+    abstract class Entity : IUpdateable, IRenderable
+    {
+        public abstract void Update();
+
+        public abstract void Render(SFML.Graphics.RenderTarget renderTarget, float extra);
+    }
+}
--- JGerdesJWiemers/JGerdesJWiemers/Game/Engine/IRenderable.cs
+++ JGerdesJWiemers/JGerdesJWiemers/Game/Engine/IRenderable.cs
@@ -7,17 +7,12 @@ using System.Threading.Tasks;
 
 namespace JGerdesJWiemers.Game.Engine
 {
-    interface IScreen
+    interface IRenderable
     {
         /// <summary>
-        /// Updates screen 
+        /// Renders on provided render target
         /// </summary>
-        void Update();
-
-        /// <summary>
-        /// Renders content of screen on provided render target
-        /// </summary>
-        /// <param name="renderTarget">target to render content of screen to</param>
+        /// <param name="renderTarget">target to render to</param>
         /// <param name="extra">value between 0 and 1 for extrapolation</param>
         void Render(RenderTarget renderTarget, float extra);
     }
