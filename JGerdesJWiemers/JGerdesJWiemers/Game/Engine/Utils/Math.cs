43932448a303027cd2d01d6bc5162d633eb3c176
Jonas Gerdes <jonas.gerdes@gmx.net>
2015-04-15 14:41:02 +0200

    added math utils

---
+++ JGerdesJWiemers/JGerdesJWiemers/Game/Engine/Utils/Math.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Window;
+
+namespace JGerdesJWiemers.Game.Engine.Utils
+{
+    public static class Math
+    {
+        public static float Length2(this Vector2f vec)
+        {
+            return vec.X * vec.X + vec.Y * vec.Y;
+        }
+
+        public static float Length(this Vector2f vec)
+        {
+            return (float) System.Math.Sqrt(vec.Length2());
+        }
+
+        public static float DistanceTo(this Vector2f v1, Vector2f v2)
+        {
+            return (v1 - v2).Length();
+        }
+
+        public static float Distance2To(this Vector2f v1, Vector2f v2)
+        {
+            return (v1 - v2).Length2();
+        }
+    }
+}
