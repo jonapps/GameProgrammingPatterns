616a8c7ada1a743c0f92c7bec20b4d1d89388540
Jonas Gerdes <jonas.gerdes@gmx.net>
2015-04-16 09:34:19 +0200

    smoother controlls

--- JGerdesJWiemers/JGerdesJWiemers/Game/Pong/Controller/Player.cs
+++ JGerdesJWiemers/JGerdesJWiemers/Game/Pong/Controller/Player.cs
@@ -3,62 +3,84 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using JGerdesJWiemers.Game.Engine.Controller;
 using SFML.Window;
+using SFML.System;
 
 namespace JGerdesJWiemers.Game.Pong.Controller
 {
     class Player : ControllerBase
     {
+        public static readonly float CONTROLLER_DEADZONE = 10;
+        public static readonly float CONTROLLER_MAX_INPUT = 100;
+        public static readonly long MILLISECONDS_TO_FULL_SPEED = 1000;
+
+        private float _controllerMovement = 0;
+        private float _keyMovement = 0;
+        private Stopwatch _stopwatch;
+
+        //true is up, false is down
         private bool _up;
-        private bool _down;
-        private bool _updated;
 
         public Player(Window w) : base(w)
         {
-            this._window.KeyPressed += this._ProcessKeyboardPress;
-            this._window.KeyReleased += this._ProcessKeyboardRelease;
-            this._updated = false;
+            _window.KeyPressed += _ProcessKeyboardPress;
+            _window.KeyReleased += _ProcessKeyboardRelease;
+            _window.JoystickMoved += _ProcessControllerInput;
+            _stopwatch = new Stopwatch();
+        }
+
+        private void _ProcessControllerInput(object sender, JoystickMoveEventArgs e)
+        {
+            if (e.Axis == Joystick.Axis.Y)
+            {
+                _controllerMovement = Math.Sign(e.Position) * e.Position * e.Position - CONTROLLER_DEADZONE;
+                _controllerMovement /= CONTROLLER_MAX_INPUT * CONTROLLER_MAX_INPUT;
+                
+            }
         }
 
         public override float Update()
         {
-            float result = 0;
-            if (_up)
+            if (_stopwatch.ElapsedMilliseconds != 0)
             {
-                result = -1; 
-            }
-            if (_down)
-            {
-                result = 1;
-            }
+                float amount = _stopwatch.ElapsedMilliseconds / (float)MILLISECONDS_TO_FULL_SPEED;
+                amount *= 2;
+                if (amount > 1)
+                    amount = 1;
 
-            return result;
+                _keyMovement = _up ? -amount : amount;
+            }
+            float currentMovement = _controllerMovement != 0 ? _controllerMovement : _keyMovement;
+            _keyMovement /= 1.2f;
+            return currentMovement;
         }
 
         private void _ProcessKeyboardPress(Object sender, KeyEventArgs e)
         {
-            if(e.Code == Keyboard.Key.W || e.Code == Keyboard.Key.Up){
-                this._up = true;
+            if(e.Code == Keyboard.Key.W || e.Code == Keyboard.Key.Up)
+            {
+                _stopwatch.Start();
+                _up = true;
             }
             if (e.Code == Keyboard.Key.S || e.Code == Keyboard.Key.Down)
             {
-                this._down = true;
+                _stopwatch.Start();
+                _up = false;
             }
         }
 
         private void _ProcessKeyboardRelease(Object sender, KeyEventArgs e)
         {
  
-            if (e.Code == Keyboard.Key.W || e.Code == Keyboard.Key.Up)
+            if (e.Code == Keyboard.Key.W || e.Code == Keyboard.Key.Up || e.Code == Keyboard.Key.S || e.Code == Keyboard.Key.Down)
             {
-                this._up = false;
-            }
-            if (e.Code == Keyboard.Key.S || e.Code == Keyboard.Key.Down)
-            {
-                this._down = false;
+                _stopwatch.Stop();
+                _stopwatch.Reset();
             }
         }
+
 
     }
 }
