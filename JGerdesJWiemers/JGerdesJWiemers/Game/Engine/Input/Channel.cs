c6604b47c04cdef69992b8d6ccccd4326908b9cb
Jonas Gerdes <jonas.gerdes@gmx.net>
2015-04-21 21:47:11 +0200

    kind of started input manager

---
+++ JGerdesJWiemers/JGerdesJWiemers/Game/Engine/Input/Channel.cs
@@ -0,0 +1,36 @@
+﻿using SFML.Window;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JGerdesJWiemers.Game.Engine.Input
+{
+    class Channel
+    {
+        struct Data
+        {
+            Keyboard.Key KeyUp;
+            Keyboard.Key KeyDown;
+            Keyboard.Key KeyLeft;
+            Keyboard.Key KeyRight;
+
+            Keyboard.Key KeyAction1;
+            Keyboard.Key KeyAction2;
+
+            Joystick.Axis AxisUpDown;
+            Joystick.Axis AxisLeftRight;
+            float UpMax;
+            float DownMax;
+            float LeftMax;
+            float RightMax;
+
+            uint ShoulderLeft;
+            uint ShoulderRight;
+            uint Action1;
+            uint Action2;
+        }
+
+    }
+}
---
+++ JGerdesJWiemers/JGerdesJWiemers/Game/Engine/Input/InputManager.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JGerdesJWiemers.Game.Engine.Input
+{
+
+    class InputManager
+    {
+
+        public delegate void MotionEventHandler(float value);
+        public delegate void ButtonEventHandler();
+
+        private List<Channel> _channel;
+
+    }
+}
