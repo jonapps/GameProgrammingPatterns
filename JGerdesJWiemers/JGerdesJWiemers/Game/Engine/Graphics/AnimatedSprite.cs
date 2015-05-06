e7c1175707212a16a769aee41f23d70a321907ea
Jonas Gerdes <jonas.gerdes@gmx.net>
2015-05-06 19:27:13 +0200

    animated sprite can be controlled with animation objects

--- JGerdesJWiemers/JGerdesJWiemers/Game/Engine/Graphics/AnimatedSprite.cs
+++ JGerdesJWiemers/JGerdesJWiemers/Game/Engine/Graphics/AnimatedSprite.cs
@@ -9,15 +9,23 @@ namespace JGerdesJWiemers.Game.Engine.Graphics
 {
     class AnimatedSprite : Sprite
     {
+        public struct Animation
+        {
+            public int[] Frames;
+            public bool Loop;
+            public bool Pingpong;
+            public int Duration;
+        }
 
         private int _tileWidth;
         private int _tileHeight;
         private int _rows;
         private int _columns;
 
+        private Queue<Animation> _animationQueue;
+
         private int _currentIndex;
         private int _timePassed;
-        private int _currentDuration;
 
         public AnimatedSprite(Texture tex, int tileWidth, int tileHeight)
             : base(tex)
@@ -28,15 +36,24 @@ namespace JGerdesJWiemers.Game.Engine.Graphics
             _columns = (int) tex.Size.Y / _tileHeight;
 
             _timePassed = 0;
-            _currentDuration = 20;
+
+            _animationQueue = new Queue<Animation>();
 
             TextureRect = new IntRect(0, 0, _tileWidth, _tileHeight);
-        }
+
+            Animation defaultAnimation = new Animation();
+            defaultAnimation.Frames = new int[] { 0 };
+            defaultAnimation.Loop = false;
+            defaultAnimation.Pingpong = false;
+            defaultAnimation.Duration = 1;
+
+            _animationQueue.Enqueue(defaultAnimation);
+        } 
 
         public new void Draw(RenderTarget renderTarget, RenderStates renderStates)
         {
             _timePassed += Game.ElapsedTime;
-            if (_timePassed >= _currentDuration)
+            if (_timePassed >= _animationQueue.Peek().Duration)
             {
                 _NextFrame();
                 _timePassed = 0;
@@ -45,18 +62,46 @@ namespace JGerdesJWiemers.Game.Engine.Graphics
 
         }
 
+
+        public void SetAnimation(Animation animation)
+        {
+            _animationQueue.Clear();
+            _animationQueue.Enqueue(animation);
+            _currentIndex = 0;
+        }
+
+        public void EnqueueAnimation(Animation animation)
+        {
+            _animationQueue.Enqueue(animation);
+        }
+
         private void _NextFrame()
         {
-            _currentIndex++;
-            if (_currentIndex >= _columns * _rows)
+            
+            //reached last frame of current animation
+            if (_currentIndex + 1 >= _animationQueue.Peek().Frames.Length)
             {
-                _currentIndex = 0;
-            }
-            int indexX = _currentIndex % _rows;
-            int indexY = _currentIndex / _rows;
-            base.TextureRect = new IntRect(indexX * _tileWidth, indexY * _tileHeight, _tileWidth, _tileHeight);
-            Console.WriteLine("indexX:"+indexX+" indexY:"+indexY);
+                //if there is another animation in queue, play it! (also remove current animation)
+                if (_animationQueue.Count > 1)
+                {
+                    _animationQueue.Dequeue();
+                    _currentIndex = 0;
+                }
+                else if (_animationQueue.Peek().Loop)
+                {
+                    _currentIndex = 0;
+                }
 
+            }
+            else
+            {
+                _currentIndex++;
+            }
+
+            int frame = _animationQueue.Peek().Frames[_currentIndex];
+            int indexX = frame % _rows;
+            int indexY = frame / _rows;
+            base.TextureRect = new IntRect(indexX * _tileWidth, indexY * _tileHeight, _tileWidth, _tileHeight);
         }
     }
 }
