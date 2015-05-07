002b11b79c7ca995b9b4f6b705dc802af3e71b2f
Jonas Gerdes <jonas.gerdes@gmx.net>
2015-05-07 11:31:52 +0200

    view follows spaceship

--- JGerdesJWiemers/JGerdesJWiemers/Game/ShootEmUp/Screens/Game.cs
+++ JGerdesJWiemers/JGerdesJWiemers/Game/ShootEmUp/Screens/Game.cs
@@ -17,13 +17,16 @@ namespace JGerdesJWiemers.Game.ShootEmUp.Screens
     class Game : GameScreen
     {
         private World _world;
+        private SpaceShip _ship;
         public Game(RenderWindow w)
             : base(w)
         {
             InputManager.Init(w);
 
             _world = new World(new Vector2(0,0));
-            _entities.Add(new SpaceShip(20,10,_world));
+
+            _ship = new SpaceShip(20, 10, _world);
+            _entities.Add(_ship);
 
             _entities.Add(new CircleEntity(40, 10, 3, _world));
             _entities.Add(new CircleEntity(45, 10, 3, _world));
@@ -45,6 +48,9 @@ namespace JGerdesJWiemers.Game.ShootEmUp.Screens
 
         public override void Render(SFML.Graphics.RenderTarget renderTarget, float extra)
         {
+            View view = renderTarget.GetView();
+            view.Center = new Vector2f(_ship.Body.Position.X, _ship.Body.Position.Y);
+            renderTarget.SetView(view);
             for (int i = 0; i < _entities.Count; ++i)
             {
                 _entities[i].Render(renderTarget, extra);
