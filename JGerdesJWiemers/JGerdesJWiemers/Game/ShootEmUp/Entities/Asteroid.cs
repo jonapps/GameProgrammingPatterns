8bff5f95942bce4725ba063c8e75ffff5ae3c54b
Jonas Gerdes <jonas.gerdes@gmx.net>
2015-05-21 19:36:37 +0200

    set max polygon vertices to 32

--- JGerdesJWiemers/JGerdesJWiemers/Game/ShootEmUp/Entities/Asteroid.cs
+++ JGerdesJWiemers/JGerdesJWiemers/Game/ShootEmUp/Entities/Asteroid.cs
@@ -17,17 +17,29 @@ namespace JGerdesJWiemers.Game.ShootEmUp.Entities
     {
         
         public Asteroid(float x, float y, World world, float scale = 1, float xSpeed = 0, float ySpeed = 0, float rotSpeed = 0) 
-            : base(AssetLoader.Instance.getTexture(AssetLoader.TEXTURE_ASTEROID1), 128, 76)
+            : base(AssetLoader.Instance.getTexture(AssetLoader.TEXTURE_ASTEROID3), 128, 76)
         {
-            Vertices vertices = new Vertices();
-            float w = ConvertUnits.ToSimUnits(scale * 128);
-            float h = ConvertUnits.ToSimUnits(scale * 76);
-            vertices.Add(new Vector2(0, 0));
-            vertices.Add(new Vector2(w, 0));
-            vertices.Add(new Vector2(w, h));
-            vertices.Add(new Vector2(0, h));
+            Vertices points = new Vertices();
+            points.Add(ConvertUnits.ToSimUnits(scale * 12.6f, scale * 18.19999f));
+            points.Add(ConvertUnits.ToSimUnits(scale * 26.19999f, scale * 12.2f));
+            points.Add(ConvertUnits.ToSimUnits(scale * 56.59998f, scale * 3.599999f));
+            points.Add(ConvertUnits.ToSimUnits(scale * 73.39997f, scale * 3.799999f));
+            points.Add(ConvertUnits.ToSimUnits(scale * 95.59997f, scale * 14.39999f));
+            points.Add(ConvertUnits.ToSimUnits(scale * 118f, scale * 9.799996f));
+            points.Add(ConvertUnits.ToSimUnits(scale * 133f, scale * 10.8f));
+            points.Add(ConvertUnits.ToSimUnits(scale * 141f, scale * 19.79999f));
+            points.Add(ConvertUnits.ToSimUnits(scale * 135.3999f, scale * 34.39999f));
+            points.Add(ConvertUnits.ToSimUnits(scale * 130.3999f, scale * 52.19998f));
+            points.Add(ConvertUnits.ToSimUnits(scale * 88.59997f, scale * 62.59998f));
+            points.Add(ConvertUnits.ToSimUnits(scale * 83.19997f, scale * 74.79997f));
+            points.Add(ConvertUnits.ToSimUnits(scale * 67.19997f, scale * 82.79997f));
+            points.Add(ConvertUnits.ToSimUnits(scale * 46.19998f, scale * 82.99997f));
+            points.Add(ConvertUnits.ToSimUnits(scale * 35.19999f, scale * 76.59998f));
+            points.Add(ConvertUnits.ToSimUnits(scale * 12.2f, scale * 55.99998f));
+            points.Add(ConvertUnits.ToSimUnits(scale * 3.799999f, scale * 32.19999f));
 
-            _Create(x, y, vertices, world);
+
+            _Create(x, y, points, world);
 
             _sprite.Scale = new Vector2f(_sprite.Scale.X * scale, _sprite.Scale.Y * scale);
 
