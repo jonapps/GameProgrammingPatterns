3d64ae4ecc2fc810b1fbb1c5c0e51d6d82749008
Jonas Gerdes <jonas.gerdes@gmx.net>
2015-06-23 18:04:05 +0200

    replaced monster assets

--- JGerdesJWiemers/JGerdesJWiemers/Game/TowerDefence/Entities/Monster.cs
+++ JGerdesJWiemers/JGerdesJWiemers/Game/TowerDefence/Entities/Monster.cs
@@ -23,24 +23,14 @@ namespace JGerdesJWiemers.Game.TowerDefence.Entities
 
         private FollowRoadAI _ai;
         private Vector2 _destination;
-        private float _speed = 1.5f;
-
-        private Animation _right;
-        private Animation _left;
-        private Animation _up;
-        private Animation _down;
+        private float _speed = 0.5f;
 
         public Monster(FarseerPhysics.Dynamics.World w, float x, float y, FollowRoadAI ai)
             : base(w, AssetLoader.Instance.getTexture(AssetLoader.TEXTURE_GUY), 0.75f)
         {
-            _right = new Animation(2 * 8, 3 * 8 - 1, 100, true, false);
-            _left = new Animation(1 * 8, 2 * 8 - 1, 100, true, false);
-            _up = new Animation(0 * 8, 1 * 8 - 1, 100, true, false);
-            _down = new Animation(3 * 8, 4 * 8 - 1, 100, true, false);
-            _sprite.SetAnimation(_right);
             _body.Position = ConvertUnits.ToSimUnits(x, y);
             _destination = _body.Position;
-
+            _sprite.Color = new Color(102, 57, 182);
             _ai = ai;
             _ai.OnDestinationChanged += OnDestinationChanged;
             _ai.OnOnDespawn += _OnOnDespawn;
@@ -56,27 +46,13 @@ namespace JGerdesJWiemers.Game.TowerDefence.Entities
         void OnDestinationChanged(Tile destination)
         {
             _destination = ConvertUnits.ToSimUnits(destination.getCenter());
-            Vector2 direction = _destination - _body.WorldCenter;
-            float distance = direction.Length();
-            direction /= distance;
+          
+        }
 
-            if (direction.X > STOPPING_DISTANCE)
-            {
-                _sprite.SetAnimation(_right);
-            }
-            if (direction.X < -STOPPING_DISTANCE)
-            {
-                _sprite.SetAnimation(_left);
-            }
-
-            if (direction.Y > STOPPING_DISTANCE)
-            {
-                _sprite.SetAnimation(_down);
-            }
-            if (direction.Y < -STOPPING_DISTANCE)
-            {
-                _sprite.SetAnimation(_up);
-            }
+        public override void PreDraw(float extra)
+        {
+            base.PreDraw(extra);
+            _sprite.Position += new Vector2f(0, (float)SMath.Sin(Game.ElapsedTime / 200f) * 5 - 5);
         }
 
         public override void Update()
